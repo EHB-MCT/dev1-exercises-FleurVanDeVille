dbd8f7f70b9a8ef06ecc3bea4ae5739c0b0cccba
Fleur Van De Ville <fleur.van.de.ville@student.ehb.be>
2022-10-06 22:02:22 +0200

    Exercise 1-3-rectangles: klaar

---
+++ exercises/1-functions/1-3-rectangles.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawnRectangle();
+
+function drawnRectangle(){
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(50, 50, 150, 150);
+    context.stroke();
+
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(200, 200, 150, 150);
+    context.stroke();
+
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(125, 125, 150, 150);
+    context.stroke();
+    context.fill();
+
+    context.strokeStyle = "black";
+    context.beginPath();
+    context.rect(50, 325, 25, 25);
+    context.stroke();
+    context.fill();
+
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(75, 275, 50, 50);
+    context.stroke();
+
+    context.strokeStyle = "black";
+    context.beginPath();
+    context.rect(325, 50, 25, 25);
+    context.stroke();
+    context.fill();
+
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(275, 75, 50, 50);
+    context.stroke();
+}
