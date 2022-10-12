d8da382b85f2f31a9fa1a97bf88152ab93ff4de6
Fleur Van De Ville <fleur.van.de.ville@student.ehb.be>
2022-10-12 12:09:12 +0200

    Exercise 2-2-randomcolour: aanpassingen klaar

--- exercises/2-variables/2-2-randomcolour.js
+++ exercises/2-variables/2-2-randomcolour.js
@@ -8,24 +8,22 @@ let context = canvas.getContext("2d");
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-let size = 400;
-
-drawRect(50, 50, 300, 300);
-drawRect(70, 70, 260, 260);
-drawRect(90, 90, 220, 220);
-drawRect(110, 110, 180, 180);
-drawRect(130, 130, 140, 140);
-drawRect(150, 150, 100, 100);
-drawRect(170, 170, 60, 60);
-drawRect(190, 190, 20, 20);
+drawRect(50, 300);
+drawRect(70, 260);
+drawRect(90, 220);
+drawRect(110, 180);
+drawRect(130, 140);
+drawRect(150, 100);
+drawRect(170, 60);
+drawRect(190, 20);
 
 
-function drawRect(x, y, size){
+function drawRect(pos, size){
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
     let color = "rgb( " + r +  "," + g + "," + b + ")";
 
     context.fillStyle = color;
-    context.fillRect( x, y, size, size);
+    context.fillRect( pos, pos, size, size);
 }
