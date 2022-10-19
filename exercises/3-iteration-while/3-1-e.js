7fa39bbdc2b62689f2e94f0944d0686fa0794777
Fleur Van De Ville <fleur.van.de.ville@student.ehb.be>
2022-10-19 12:19:01 +0200

    Exercise: 3-1-e: bezig

--- exercises/3-iteration-while/3-1-e.js
+++ exercises/3-iteration-while/3-1-e.js
@@ -13,10 +13,22 @@ function draw() {
 }
 
 function drawDiagonalGrid() {
-    let i = ;
-    while (i <= ) {
-        Utils.drawLine(, , , );
+    let i = 0;
+    while (i <= 5) {
+        let x = ;
+        let y = ;
+        Utils.drawLine(50, 200 , , );
         Utils.drawLine(, , , );
         i = ;
     }
+}
+
+let i = 0;
+while (i < 6) {
+    let x = 75 + i * 50;
+    let y = x;
+    console.log(i);
+    Utils.drawLine(75, y , 325, y);
+    Utils.drawLine(x, 75, x, 325);
+    i++ ;
 }
