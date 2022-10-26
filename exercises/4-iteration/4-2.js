c8a14f119e290428e0fb8bec6825118d9616bb7a
Fleur Van De Ville <fleur.van.de.ville@student.ehb.be>
2022-10-26 12:14:55 +0200

    Exercise: 4-2: klaar

--- exercises/4-iteration/4-2.js
+++ exercises/4-iteration/4-2.js
@@ -3,25 +3,21 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
-let width = context.canvas.width;
-let height = context.canvas.height;
+let width = context.canvas.width - 400;
+let height = context.canvas.height - 400;
 
 drawnBubbles();
 
 function drawnBubbles() {
     for (let i = 0; i < 80; i++) {
         context.beginPath();
-        let r = Math.floor(Math.random() * 255);
-        let g = Math.floor(Math.random() * 255);
-        let b = Math.floor(Math.random() * 255);
-        console.log(r, g, b);
-        let color = "rgb( " + r +  "," + g + "," + b + ")";
-        let radius = Math.random() * 40;
-        let x = Math.random() * width - 200;
-        let y = Math.random() * height - 200;
-        context.ellipse(x, y, radius, radius, 0, 0, 360);
+        let h = Math.random() * 255;
+        let color = "hsla(" + h +", 100%, 50%, .5)";
+        let radius = Math.random() * 50;
+        let x = Math.random() * width;
+        let y = Math.random() * height;
+        context.ellipse(x + 200, y + 200, radius, radius, 0, 0, 360);
         context.fillStyle = color;
         context.fill();
-
     }
 }
